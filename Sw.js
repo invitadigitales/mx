a4bab5228d448d8cba39e5e5001050376f8c4c8c
GusDev <gustaavomeza@gmail.com>
2026-03-22 01:56:35 -0600

    s

--- Sw.js
+++ Sw.js
@@ -1,3 +1,14 @@
+self.addEventListener("push", (event) => {
+  const data = event.data ? event.data.json() : { titulo: "Nuevo mensaje", cuerpo: "" };
+  event.waitUntil(
+    self.registration.showNotification(data.titulo, {
+      body: data.cuerpo,
+      vibrate: [200, 100, 200],
+      tag: "mensaje-chat"
+    })
+  );
+});
+
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   event.waitUntil(clients.openWindow("/"));
